3ef6d2f704461b9f76c1b9c9f9f730e934ea1d97
Pesach Bagad <bgdpsh@gmail.com>
2026-09-04 10:05:38 +0300

    handle comsumer and writing to mongoDb database

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.IO;
+using Confluent.Kafka;
+using Consumer.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    var consumerConfig = new ConsumerConfig();
+    configuration.GetSection("Kafka:Consumer").Bind(consumerConfig);
+    services.AddSingleton(consumerConfig);
+
+    services.AddSingleton<IMongoClient>(sp =>
+        new MongoClient(configuration["MongoDb:ConnectionString"]));
+
+    services.AddSingleton<IMongoDatabase>(sp =>
+    {
+        var client = sp.GetRequiredService<IMongoClient>();
+        return client.GetDatabase(configuration["MongoDb:DatabaseName"]);
+    });
+
+    services.AddHostedService<MongoConsumerServices>();
+});
+
+await builder.Build().RunAsync();
---
+++ Consumer/Services/MongoConsumerServices.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Confluent.Kafka;
+
+namespace Consumer.Services;
+
+public class MongoConsumerServices: BackgroundService
+{
+    private readonly ConsumerConfig _consumerConfig;
+    private readonly IMongoCollection<BsonDocument> _collection;
+    private readonly string _topic;
+
+    public MongoConsumerServices(
+        ConsumerConfig consumerConfig,
+        IMongoDatabase database,
+        IConfiguration configuration)
+    {
+        _consumerConfig = consumerConfig;
+        _collection = database.GetCollection<BsonDocument>("CleanAnswers");
+        _topic = configuration["Kafka:Topic"]!;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Task.Run(async () =>
+        {
+            using var consumer = new ConsumerBuilder<Ignore, string>(_consumerConfig).Build();
+            consumer.Subscribe(_topic);
+
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    ConsumeResult<Ignore, string>? consumeResult = null;
+
+                    try
+                    {
+                        consumeResult = consumer.Consume(stoppingToken);
+                    }
+                    catch (OperationCanceledException) { break; }
+                    catch (ConsumeException ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Error.Reason}");
+                        continue;
+                    }
+
+                    if (consumeResult?.Message?.Value == null || string.IsNullOrWhiteSpace(consumeResult.Message.Value)){  continue; }
+
+                    try
+                    {
+                        var document = BsonDocument.Parse(consumeResult.Message.Value);
+
+                        await _collection.InsertOneAsync(document, cancellationToken: stoppingToken);
+
+                        consumer.Commit(consumeResult);
+                    }
+                    catch (FormatException jsonEx)
+                    {
+                        Console.WriteLine($"Error in JSON: {jsonEx.Message}");
+                        consumer.Commit(consumeResult);
+                    }
+                    catch (MongoException mongoEx)
+                    {
+                        Console.WriteLine($"Error in saving to MongoDB: {mongoEx.Message}");
+                    }
+                }
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }, stoppingToken);
+    }
+}
+
+
